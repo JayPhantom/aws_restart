beb3ac3326f1936b34183a0cd961e9892cd979df
JayPhantom <109772529+JayPhantom@users.noreply.github.com>
2023-11-15 12:23:35 +0800

    Updated prime2.py
    converted captured prime numbers from int to string
    
    included command to store the captured prime numbers into results2.txt

--- prime2.py
+++ prime2.py
@@ -1,7 +1,29 @@
+
+#declare array to place prime numbers
+store_prime = []
+
+#iterate between prime numbers
 for i in range(2,250):
     prime = True
     for j in range(2,i):
         if (i%j==0):
-            prime = False
-    if prime == True:
-       print (i)
+            break
+    else:
+       #store prime numbers in an array
+       store_prime.append(i)
+
+#convert saved prime numbers from int to string
+x = str(store_prime)
+
+#declare two variables
+string_prime=''
+content =" "
+
+#command to save converted prime numbers to results2.txt
+textfile = open('/Users/Jacob/Documents/AWS Resstart/results2.txt', 'w')
+
+#write saved prime numbers
+textfile.writelines(string_prime.join(x))
+
+
+
